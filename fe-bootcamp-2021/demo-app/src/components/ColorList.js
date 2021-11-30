a81e96119a6fd869064b59cbe16ee3125cae4508
Malik, Buland <Buland_Malik@intuit.com>
2021-11-30 11:49:09 -0800

    updates

--- fe-bootcamp-2021/demo-app/src/components/ColorList.js
+++ fe-bootcamp-2021/demo-app/src/components/ColorList.js
@@ -1,15 +1,36 @@
+const isEmpty = arr => !Array.isArray(arr) || arr.length ===0;
+const isEmpty_1 = (arr) => !Array.isArray(arr) || arr.length ===0;
+
 export const ColorList = (props) => {
+
+    /*
+    return (
+        <>
+            <ul>
+                {( !Array.isArray(props.colors) || props.colors.length ===0) 
+                    ? <li>No Colors</li> 
+                    : props.colors.map( color =>
+                        <li key={color.id}>
+                            {color.name} - {color.hexcode}
+                        </li>)
+                }
+            </ul>        
+        </>
+    );*/
 
     return (
         <>
             <ul>
-                {props.colors.map( color =>
-                    <li key={color.id}>
-                        {color.name} - {color.hexcode}
-                    </li>)
+                { 
+                    isEmpty(props.colors) 
+                    ? <li>No Colors</li> 
+                    : props.colors.map( color =>
+                        <li key={color.id}>
+                            {color.name} - {color.hexcode}
+                        </li>)
                 }
             </ul>        
         </>
-    );
+    );    
 
 };
